15a8e40dcbd9a7cdf26f05ecfe016e3d6140031d
Ricardo Navarro <r.navarro@blackbullmarkets.com>
2024-01-29 10:04:29 -0600

    First Commit

---
+++ act-1/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    if (process.env.NODE_ENV === 'dev') {
+        console.log(`App is running on port ${port}`)
+    } else {
+        console.log(`App is running`)
+    }
+})
