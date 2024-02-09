221621192570beec8b1607ccc724e9aca18facc8
Ricardo Navarro <r.navarro@blackbullmarkets.com>
2024-02-08 22:44:35 -0600

    Finished activity

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ITESO-backend-2
+
+Este repositorio contendrá las actividades, tareas y prácticas para la materia de Backend 2. Para abrir cualquier tarea, por favor ejecuta en la terminal el nombre de la carpeta y sigue el respectivo README.md.
+
+Por ejemplo:
+
+```bash
+cd tarea1
+```
+
+Esto te llevará al directorio de la tarea 1, donde encontrarás el archivo README.md con las instrucciones correspondientes.
---
+++ tarea1/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST || 'localhost'
+
+const users = require('./routes/users')
+
+app.get('/healthcheck', (req, res) => {
+    res.send('Working Fine Pal!')
+})
+
+app.use('/users', users)
+
+app.listen(port, () => {
+    if (process.env.NODE_ENV === 'dev') {
+        console.log(`App is running on ${host}:${port}`)
+    } else {
+        console.log(`App is running on Production mode`)
+    }
+})
