906c5e2c0cb5e699fa07d814abeebd7dcbaba0f4
Darksw20 <ricardog.navi20@gmail.com>
2024-03-10 13:37:58 -0600

    Scaffolding of the project working for dev and prod

--- tarea4/backend/index.js
+++ tarea4/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-require('dotenv').config();
+require('dotenv').config({ path: `../.env.${process.env.NODE_ENV}` });
+const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -14,22 +15,26 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/public', express.static('public'));
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
 app.use('/api', routes);
 
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
+const port = process.env.BACK_PORT || 3000;
+const host = process.env.BACK_HOST || 'localhost';
 const db = process.env.DB_URI;
 
 mongoose.connect(db)
     .then(() => {
         console.log('Connected to the database');
         app.listen(port, () => {
-            if (process.env.NODE_ENV === 'dev') {
+            if (process.env.NODE_ENV === 'development') {
                 console.log(`App is running on ${host}:${port}`);
             } else {
-                console.log(`App is running on Production mode`);
+                console.log(`App is running on Production mode on ${host}:${port}`);
             }
         })
     })
